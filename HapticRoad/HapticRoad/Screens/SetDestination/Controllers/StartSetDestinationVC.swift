26601aa5cf0f77564e46ea3cc2992e0a3843a125
hwangJi-dev <melon2228@swu.ac.kr>
2021-12-30 03:06:52 +0900

    ➕[ADD] : 필요한 파일 추가

---
+++ HapticRoad/HapticRoad/Screens/SetDestination/Controllers/StartSetDestinationVC.swift
@@ -0,0 +1,24 @@
+//
+//  StartSetDestinationVC.swift
+//  HapticRoad
+//
+//  Created by 황지은 on 2021/12/29.
+//
+
+import UIKit
+
+class StartSetDestinationVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - IBAction
+    @IBAction func startWayFindingBtnDidTap(_ sender: UIButton) {
+        print("start")
+    }
+    
+    @IBAction func stopWayFindingBtnDidTap(_ sender: UIButton) {
+        print("stop")
+    }
+}
