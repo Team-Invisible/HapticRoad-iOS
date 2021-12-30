8b601f3a87fec76aa32989a5435da5efd56263d2
hwangJi-dev <melon2228@swu.ac.kr>
2021-12-31 03:54:44 +0900

    ➕[ADD] : 에셋 추가 및 주소 변환 TVC 관련 파일 생성

---
+++ HapticRoad/HapticRoad/Network/APIModels/AddressData.swift
@@ -0,0 +1,8 @@
+//
+//  AddressData.swift
+//  HapticRoad
+//
+//  Created by 황지은 on 2021/12/30.
+//
+
+import Foundation
---
+++ HapticRoad/HapticRoad/Screens/SetDestination/Cells/AdressTVC.swift
@@ -0,0 +1,23 @@
+//
+//  AdressTVC.swift
+//  HapticRoad
+//
+//  Created by 황지은 on 2021/12/30.
+//
+
+import UIKit
+
+class AdressTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
